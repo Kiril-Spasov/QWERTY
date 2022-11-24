d496d9e6d8a36886268daf4ce6002a536ee8fb5c
Kiril Spasov <spasov.kiril@gmail.com>
2022-11-24 18:45:34 +0200

    Add project files.

---
+++ QWERTY/FormQwerty.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace QWERTY
+{
+    partial class FormQwerty
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnTranslate = new System.Windows.Forms.Button();
+            this.TxtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnTranslate
+            // 
+            this.BtnTranslate.Location = new System.Drawing.Point(49, 57);
+            this.BtnTranslate.Name = "BtnTranslate";
+            this.BtnTranslate.Size = new System.Drawing.Size(182, 68);
+            this.BtnTranslate.TabIndex = 0;
+            this.BtnTranslate.Text = "Translate";
+            this.BtnTranslate.UseVisualStyleBackColor = true;
+            this.BtnTranslate.Click += new System.EventHandler(this.BtnTranslate_Click);
+            // 
+            // TxtResult
+            // 
+            this.TxtResult.Location = new System.Drawing.Point(304, 57);
+            this.TxtResult.Multiline = true;
+            this.TxtResult.Name = "TxtResult";
+            this.TxtResult.Size = new System.Drawing.Size(410, 339);
+            this.TxtResult.TabIndex = 1;
+            // 
+            // FormQwerty
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TxtResult);
+            this.Controls.Add(this.BtnTranslate);
+            this.Name = "FormQwerty";
+            this.Text = "Qwerty";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnTranslate;
+        private System.Windows.Forms.TextBox TxtResult;
+    }
+}
+
---
+++ QWERTY/FormQwerty.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace QWERTY
+{
+    public partial class FormQwerty : Form
+    {
+        public FormQwerty()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnTranslate_Click(object sender, EventArgs e)
+        {
+            string line = "";
+
+            string path = Application.StartupPath + @"\qwerty.txt";
+            StreamReader streamReader = new StreamReader(path);
+
+            line = streamReader.ReadLine();
+
+            TxtResult.Text = CorrectCaseSensitivity(Translate(line));
+
+        }
+
+        private string Translate(string text)
+        {
+            string dvorak = "`1!234567890[]'<.PYFGCRL?+AOEUIDHTNS-;QJKXBMWVZ";
+            string qwerty = "`1!234567890-=QWERTYUIOP[]ASDFGHJKL;'ZXCVBNM<>?";
+
+            string result = "";
+            string input;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                input = text.Substring(i, 1);
+
+                if (input == " ")
+                {
+                    result += " ";
+                }
+
+                for (int j = 0; j < dvorak.Length; j++)
+                {
+                    if (input == dvorak[j].ToString())
+                    {
+                        result += qwerty[j];
+                    }
+                    else if (input == dvorak[j].ToString().ToLower())
+                    {
+                        result += qwerty[j].ToString().ToLower();
+                    }
+                }
+            }
+            return result;
+        }
+
+        private string CorrectCaseSensitivity(string text)
+        {
+            string letter;
+            string result = "";
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                
+                letter = text.Substring(i, 1);
+
+                if (i == 0)
+                {
+                    result += letter.ToUpper();
+                }
+                else if (letter == " ")
+                {
+                    result += " " + text.Substring(i + 1, 1).ToUpper();
+                    i++;
+                }
+                else
+                {
+                    result += letter.ToLower();
+                }
+            }
+
+            return result;
+        }
+    }
+}
